43c060c6b193a49368c03f23cc756657f589d4f6
Mohamed alshami <mo.mohamed45987@gmail.com>
2023-04-26 04:53:15 +0200

    Finished favoriting functionality

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export async function POST(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  // if current user does not have any id just spread and empty array
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+export async function DELETE(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  // if current user does not have any id just spread and empty array
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId); // delete our listing id from fav array
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { safeUser } from "../types";
+import useFavorite from "../Hooks/useFavorite";
 
 interface HeartButtonProps {
   listingId: string;
@@ -12,8 +13,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
 
   return (
     <div
