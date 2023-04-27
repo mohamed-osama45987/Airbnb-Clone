5598b74bde90c000933829bffd5b0a9f171b05ac
Mohamed alshami <mo.mohamed45987@gmail.com>
2023-04-27 22:30:48 +0200

    Finished individual listing page

---
+++ app/actions/getListingById.ts
@@ -0,0 +1,38 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export default async function getListingById(params: IParams) {
+  try {
+    const { listingId } = params;
+
+    const listing = await prisma.listings.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!listing) {
+      return null;
+    }
+
+    // to prevent the warings about sending date obj we need to sanitize the returned listing
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toISOString(),
+        updatedAt: listing.user.updatedAt.toISOString(),
+        emailVerified: listing.user.emailVerified?.toISOString() || null,
+      },
+    };
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+
+import useContries from "@/app/Hooks/useCountries";
+import { safeUser } from "@/app/types";
+import Heading from "../Heading";
+import HeartButton from "../HeartButton";
+
+interface LisitingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: safeUser | null;
+}
+
+const ListingHead: React.FC<LisitingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}) => {
+  const { getByValue } = useContries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          alt="Listing image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingHead;
